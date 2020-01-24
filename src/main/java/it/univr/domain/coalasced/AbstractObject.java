cd3ba8651f6b5056a9a472d56167cde02cab0d91
VincenzoArceri <vincenzoarceri.92@gmail.com>
2020-01-24 11:34:55 +0100

    testNormalalization012 fails

--- src/main/java/it/univr/domain/coalasced/AbstractObject.java
+++ src/main/java/it/univr/domain/coalasced/AbstractObject.java
@@ -1,41 +1,37 @@
 package it.univr.domain.coalasced;
 
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.SortedSet;
 
+import org.antlr.v4.runtime.misc.Pair;
 import org.apache.commons.collections15.multimap.MultiHashMap;
+
 import it.univr.domain.AbstractValue;
 import it.univr.fsm.machine.Automaton;
 
 public class AbstractObject implements AbstractValue {
-	
+
 	private MultiHashMap<FA, AbstractValue> abstractObject;
-	
+
 	public AbstractObject(FA fa, AbstractValue abstractValue) {
 		this.abstractObject = new MultiHashMap<>();
 		this.abstractObject.put(fa, abstractValue);
 	}
-	
+
 	public AbstractObject() {
 		this.abstractObject = new MultiHashMap<>();
 	}
-	
+
 	public MultiHashMap<FA, AbstractValue> getAbstractObjectMap() {
 		return this.abstractObject;
 	}
-	
+
 	public AbstractObject(MultiHashMap<FA, AbstractValue> abstractObject) {
 		this.abstractObject = new MultiHashMap<>();
 		this.abstractObject.putAll(abstractObject);
 	}
-	
+
 	@Override
 	public AbstractValue juggleToNumber() {
 		// TODO: Marin
@@ -84,12 +80,12 @@ public class AbstractObject implements AbstractValue {
 		// TODO: Marin
 		return null;
 	}
-	
+
 	@Override
 	public String toString() {
 		return abstractObject.toString();
 	}
-	
+
 	@Override
 	public int hashCode() {
 		// TODO: Marin
@@ -100,10 +96,28 @@ public class AbstractObject implements AbstractValue {
 	public boolean equals(Object other) {
 		if (other instanceof AbstractObject)
 			return this.abstractObject.equals(((AbstractObject)other).getAbstractObjectMap());
-		
+
 		return false;
 	}
-	
+
+	public void put(FA k, AbstractValue v) {
+		if (this.abstractObject.containsKey(k)) {
+			AbstractValue toPut = new Bottom();
+			ArrayList<AbstractValue> other = (ArrayList<AbstractValue>) this.abstractObject.get(k);
+			abstractObject.remove(k);
+
+			for (AbstractValue o : other)
+				toPut = toPut.leastUpperBound(o);
+
+			abstractObject.put(k, v.leastUpperBound(toPut));
+
+		} else {
+			this.abstractObject.put(k, v);
+
+		}
+
+	}
+
 	/**
 	 * 
 	 * @return normalized this abstract object
@@ -111,94 +125,119 @@ public class AbstractObject implements AbstractValue {
 	public void normalize() {
 
 		HashSet<FA> keys = new HashSet<FA>();
-		
-		for (FA k : this.abstractObject.keySet())
+
+		for (FA k : this.abstractObject.keySet()) {
 			keys.add(k.clone());
+		}
 
 		// first part
 		for (FA abstractProperty: keys) {
 			Collection<AbstractValue> abstractValue = getAbstractObjectMap().get(abstractProperty);
-			
+
 			if (!abstractProperty.isInfinite() && (!abstractProperty.isSingleString() || abstractProperty.getLanguage().size() != 1)) {
 				// this means that the abstract property recognizes only finite languages (not equals to 1)
+
 				abstractObject.remove(abstractProperty);
 				for (String s: abstractProperty.getLanguage())
 					for(AbstractValue a: abstractValue)
-						this.abstractObject.put(new FA(s), a);
+						put(new FA(s), a);
 			}
 		}
-		
+
 		// second part
-		LinkedHashSet<FA> properties = new LinkedHashSet<>(this.abstractObject.keySet());
-		
-		while (properties.size() > 0) {
-			FA abstractProperty1 = (FA)properties.toArray()[0];
-			
-			AbstractValue abstractValue1 =  this.lookupAbstractObject(abstractProperty1);
-			this.abstractObject.remove(abstractProperty1);
-			properties.remove(abstractProperty1);
-			
-			boolean normalized = false;
-			
-			for (int i = 0; i < properties.size(); i++) {
-				FA abstractProperty2 = (FA)properties.toArray()[i];
-				
-				AbstractValue abstractValue2 = this.lookupAbstractObject(abstractProperty2);
-				Automaton intersectionAutomaton = Automaton.intersection(abstractProperty1.getAutomaton(), abstractProperty2.getAutomaton());
-				if ((!Automaton.isEmptyLanguageAccepted(intersectionAutomaton)) && !abstractProperty1.equals(abstractProperty2)) {
-					normalized = true;
-					FA intersectionProperty = new FA(intersectionAutomaton);
-					
-					this.abstractObject.put(intersectionProperty, this.lookupAbstractObject(intersectionProperty).leastUpperBound(abstractValue1).leastUpperBound(abstractValue2));
-					properties.add(intersectionProperty);
-					
-					FA minusP1P2 = abstractProperty1.minus(abstractProperty2);
-					if (!Automaton.isEmptyLanguageAccepted(minusP1P2.getAutomaton())) {
-						this.abstractObject.put(minusP1P2, this.lookupAbstractObject(minusP1P2).leastUpperBound(abstractValue1));
-						properties.add(minusP1P2);
+		int previousHash;
+		do {
+			HashSet<Pair<FA, FA>> visited = new HashSet<Pair<FA,FA>>();
+
+			previousHash = abstractObject.hashCode();
+			keys = new HashSet<FA>();
+
+			for (FA k : this.abstractObject.keySet()) 
+				keys.add(k.clone());
+									
+			for (FA k1 : keys)  {
+
+				AbstractValue v1 = get(k1);
+
+				for (FA k2 : keys)  {
+
+					AbstractValue v2 = get(k2);
+
+					Automaton intersectionAutomaton = Automaton.intersection(k1.getAutomaton(), k2.getAutomaton());
+					Pair<FA,FA> toCheck1 = new Pair<FA, FA>(k1, k2);
+					Pair<FA,FA> toCheck2 = new Pair<FA, FA>(k2, k1);
+
+					if ((!Automaton.isEmptyLanguageAccepted(intersectionAutomaton)) && !k1.equals(k2) && !visited.contains(toCheck1) && !visited.contains(toCheck2)) {
+
+						FA intersectionProperty = new FA(intersectionAutomaton);
+						this.abstractObject.remove(k1);
+						this.abstractObject.remove(k2);
+						
+						FA minusP1P2 = k1.minus(k2); AbstractValue minusP1P2Value = get(minusP1P2).leastUpperBound(v1);
+						FA minusP2P1 = k2.minus(k1); AbstractValue minusP2P1Value = get(minusP2P1).leastUpperBound(v2);
+						
+						MultiHashMap<FA, AbstractValue> toAdd = new MultiHashMap<FA, AbstractValue>();
+
+						toAdd.put(intersectionProperty, lookupAbstractObject(intersectionProperty).leastUpperBound(v1).leastUpperBound(v2));
+
+
+						if (!Automaton.isEmptyLanguageAccepted(minusP1P2.getAutomaton()) && !minusP1P2Value.equals(new Bottom())) 
+							toAdd.put(minusP1P2, minusP1P2Value);
+												
+						if (!Automaton.isEmptyLanguageAccepted(minusP2P1.getAutomaton()) && !minusP2P1Value.equals(new Bottom())) 
+							toAdd.put(minusP2P1, minusP2P1Value);
+						
+						
+						for (FA k : toAdd.keySet()) 
+							for (AbstractValue v : toAdd.get(k))
+								put(k, v); 
+
 					}
-					FA minusP2P1 = abstractProperty2.minus(abstractProperty1);
-					if (!Automaton.isEmptyLanguageAccepted(minusP2P1.getAutomaton())) {
-						this.abstractObject.put(minusP2P1, this.lookupAbstractObject(minusP2P1).leastUpperBound(abstractValue2));
-						properties.add(minusP2P1);
-					}
-					this.abstractObject.remove(abstractProperty2);
-					properties.remove(abstractProperty2);
-					i--;
+					visited.add(new Pair<FA, FA>(k1, k2));
+					visited.add(new Pair<FA, FA>(k2, k1));
 				}
 			}
-			if (!normalized) {
-				this.abstractObject.put(abstractProperty1, abstractValue1);
-			}
-		}
-		
+			
+		} while (previousHash != abstractObject.hashCode());
+
 	}
-	
+
+
 	public AbstractValue lookupAbstractObject(FA p) {
 		//TODO: Marin
-		
+
 		AbstractValue resultAbstractValue = new Bottom();
-		
-	
-		
+
+
+
 		for (FA abstractProperty: getAbstractObjectMap().keySet()) {
 			// for each abstract property
 			if (!Automaton.intersection(p.getAutomaton(), abstractProperty.getAutomaton()).equals(Automaton.makeEmptyLanguage())) {
-				
+
 				for(Object o: (Collection<?>) getAbstractObjectMap().get(abstractProperty)) {
-					
+
 					if(o instanceof AbstractValue) {
-						
-					
-						
+
+
+
 						resultAbstractValue = resultAbstractValue.leastUpperBound((AbstractValue)o);
-						
+
 					}
 				}
 			}
 		}
-		
+
 		return resultAbstractValue;
-	
+
 	}
+
+	public AbstractValue get(FA k) {
+		AbstractValue v2 = new Bottom();
+		ArrayList<AbstractValue> s = (ArrayList<AbstractValue>) abstractObject.get(k);
+		if (s != null)
+			for (AbstractValue v : s)
+				v2 = v;
+		return v2;
+	}
+
 }
