3544fbc6f12233eab798b8dac620719fe0e9475a
Daniel <daniel.tinazzi@pm.me>
2020-02-05 16:30:19 +0100

    visitObjectAsg

--- src/main/java/it/univr/main/AbstractInterpreter.java
+++ src/main/java/it/univr/main/AbstractInterpreter.java
@@ -1,11 +1,13 @@
 package it.univr.main;
 
 import org.antlr.v4.runtime.misc.NotNull;
+import org.hamcrest.core.IsInstanceOf;
 import org.omg.CORBA.Environment;
 
 import it.univr.domain.AbstractDomain;
 import it.univr.domain.AbstractValue;
 import it.univr.domain.AllocationSite;
+import it.univr.domain.coalasced.AbstractObject;
 import it.univr.domain.coalasced.AllocationSites;
 import it.univr.domain.coalasced.Bool;
 import it.univr.domain.coalasced.Bottom;
@@ -94,8 +96,10 @@ public class AbstractInterpreter extends MuJsBaseVisitor<AbstractValue> {
 	public AbstractValue visitObjectAsg(MuJsParser.ObjectAsgContext ctx) { 
 		// TODO: Marin
 		AbstractValue obj = visit(ctx.object());
-		env.getStore().put(new Variable("x"), new AllocationSites(getProgramPoint()));
-		env.getHeap().put(getProgramPoint(), visit(ctx.object()));
+		if (obj instanceof AbstractObject) {			
+			env.getStore().put(new Variable("x"), new AllocationSites(getProgramPoint()));
+			env.getHeap().put(getProgramPoint(), visit(ctx.object()));
+		}
 		
 		return new Bottom();
 	}
