d3b2bfed0c5e98a41e4c35fd0f19f3f47b2e7519
VincenzoArceri <vincenzoarceri.92@gmail.com>
2020-02-10 16:41:12 +0100

    Preparing tests for widening

---
+++ src/test/java/it/univr/main/coalesced/CoalescedObjectWideningTest.java
@@ -0,0 +1,8 @@
+package it.univr.main.coalesced;
+
+import it.univr.domain.coalasced.CoalascedAbstractDomain;
+
+public class CoalescedObjectWideningTest {
+	private String dir = "src/test/resources/objects/widening";
+	private CoalascedAbstractDomain domain = new CoalascedAbstractDomain();
+}
